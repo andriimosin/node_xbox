b07d2c77cc0663399fe8a055f3cdef83acc63819
Valentine Ostakh <valikos.ost@gmail.com>
2013-08-18 04:03:11 +0300

    correct flying front/back

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ var arDrone  = require("ar-drone"),
     angle    = 64,
     speed    = 0.4;
 
-client.config('control:altitude_max', 100000)
-client.config('control:control_vz_max', 1000)
-client.config('control:control_yaw', 4.0)
-client.config('control:euler_angle_max', 0.3)
+client.config('control:altitude_max', 100000);
+client.config('control:control_vz_max', 1000);
+client.config('control:control_yaw', 4.0);
+client.config('control:euler_angle_max', 0.3);
 
 xbox.on('a:press', function (key) {
   console.log(key + ' press');
@@ -18,59 +18,101 @@ xbox.on('a:press', function (key) {
 
 xbox.on('b:press', function (key) {
   console.log(key + ' press');
-  client.land();
-});
-
-xbox.on('b:release', function (key) {
-    console.log('b release');
-});
-
-
-// xbox.on('lefttrigger', function(position){
-//   console.log('lefttrigger', position)
-// })
-
-// xbox.on('righttrigger', function(position){
-//   console.log('righttrigger', position)
-// })
-
-xbox.on('left:move', function(position){
-  console.log('left:move', position)
+  client.land(function(){
+    console.log('landed');
+    client.stop();
+  });
 });
 
 xbox.on('left:move', function(position){
 
-  // fly forward-back
-  // max 32767
-  // min 32767
+  var dead = 2;
 
-  var deadArea = 10000;
-  
-  // client.front(speed);
-  // client.back(speed);
+  if (position.x <= dead) {
+    var val = (dead - position.x) / angle * speed;
+    console.log("left:", val, position);
+    client.left(val);
+  } else if (position.x > dead) {
+    var val = (position.x - dead) / angle * speed;
+    console.log("right:", val, position);
+    client.right(val);
+  };
 
-  if (position.y <= -deadArea) {
-    console.log("front:", (position.y) / angle * speed);
-    client.front(Math.abs(position.y) / angle * speed);
-  } else if (position.y > deadArea) {
-    console.log("back:", (position.y) / angle * speed);
-    client.back(Math.abs(position.y) / angle * speed);
+
+  if (position.y <= dead) {
+    var val = (dead - position.y) / angle * speed;
+    console.log("front:", val, position);
+    client.front(val);
+  } else if (position.y > dead) {
+    var val = (position.y - dead) / angle * speed;
+    console.log("back:", val, position);
+    client.back(val);
+  };
+});
+
+xbox.on('right:move', function(position) {
+
+  var dead = 2;
+
+  if (position.y <= dead) {
+    var val = (dead - position.y) / angle * speed;
+    console.log("up:", val, position);
+    client.up(val);
+  } else if (position.y > dead) {
+    var val = (position.y - dead) / angle * speed;
+    console.log("down:", val, position);
+    client.down(val);
   }
 
-  // if (position.x < 0) {
-  //   console.log("left:", (position.x) / angle * speed);
-  //   client.left(Math.abs(position.x) / angle * speed);
-  // }
-  // if (position.x >= 0) {
-  //   console.log("right:", (position.x) / angle * speed);
-  //   client.right(Math.abs(position.x) / angle * speed);
-  // }
+  if (position.x <= dead) {
+    var val = (dead - position.x) / angle * speed;
+    console.log("counterclockwise:", val, position);
+    client.counterClockwise(val);
+  } else if (position.x > 64) {
+    var val = (position.x - dead) / angle * speed;
+    console.log("clockwise:", dead, position);
+    client.clockwise(val);
+  }
 });
 
-// if (type == "left" && value.x <= 128) {
-//     console.log("left:", (128 - value.x) / angle * speed);
-//     client.left((128 - value.x) / angle * speed);
-// } else if (type == "left" && value.x > 128) {
-//     console.log("right:", (value.x - 128) / angle * speed);
-//     client.right((value.x - 128) / angle * speed);
-// }
+/* TODO: understand this options for ar-drone
+
+var arDrone = require('ar-drone');
+var client = arDrone.createUdpControl();
+var start   = Date.now();
+
+var ref  = {};
+var pcmd = {};
+
+console.log('Recovering from emergency mode if there was one ...');
+ref.emergency = true;
+
+setTimeout(function() {
+  console.log('Takeoff ...');
+
+  ref.emergency = false;
+  ref.fly       = true;
+
+}, 1000);
+
+setTimeout(function() {
+  console.log('Turning clockwise ...');
+
+  pcmd.clockwise = 0.5;
+}, 6000);
+
+setTimeout(function() {
+  console.log('Landing ...');
+
+  ref.fly = false;
+  pcmd = {};
+}, 8000);
+
+
+setInterval(function() {
+  client.ref(ref);
+  client.pcmd(pcmd);
+  client.flush();
+}, 30);
+
+*/
